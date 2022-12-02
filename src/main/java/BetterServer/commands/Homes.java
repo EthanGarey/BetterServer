d396b36a7241e19022be30579b6e4026d1969a1e
EthanGarey <ethgarey@gmail.com>
2022-12-02 16:36:03 -0500

    Version 0.0.3.4.5 (Added homes! This comes with 4 new features! /home, /sethome, /delhome & a home.yml file!)

--- src/main/java/BetterServer/commands/Homes.java
+++ src/main/java/BetterServer/commands/Homes.java
@@ -2,17 +2,24 @@ package BetterServer.commands;
 
 import BetterServer.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 
-@SuppressWarnings("ALL")
+import java.io.File;
+import java.io.IOException;
+import java.util.StringJoiner;
+
 public class Homes implements CommandExecutor{
 
-    final Main plugin;
     final FileConfiguration homeConfig = new YamlConfiguration();
+    final Main plugin;
 
 
     public Homes(Main plugin) {
@@ -20,18 +27,18 @@ public class Homes implements CommandExecutor{
         if (plugin.getConfig().getInt("MaxPlayerHomes") == 0) {
             Bukkit.getConsoleSender().sendMessage("§4§lError, Configuration \"MaxPlayerHome\" must be greater than 0");
         }
-/*        load();
-        createHomesFile();*/
+        createHomesFile();
+        load();
+
         plugin.getCommand("home").setExecutor(this);
         plugin.getCommand("sethome").setExecutor(this);
         plugin.getCommand("delhome").setExecutor(this);
+
     }
 
-
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        sender.sendMessage("This command is being worked on!");
-        return true;
-        /*
+
+
         ifItExists();
         load();
         if (sender instanceof Player player) {
@@ -47,9 +54,10 @@ public class Homes implements CommandExecutor{
                         checkifplayerhashome(player, homename);
                     } else {
 
-                        player.sendMessage("Here is a list of your homes!");
+                        player.sendMessage("§a§lHere is a list of your homes.");
+
                         listhomes(player);
-                        
+
                     }
 
                 }
@@ -60,7 +68,6 @@ public class Homes implements CommandExecutor{
                     }
                     if (args.length >= 1) {
                         String homename = args[0];
-                        int maxhomes = plugin.getConfig().getInt("MaxPlayerHomes");
                         if (! homeConfig.getStringList("Homes.Players." + player.getUniqueId()).contains(homename)) {
                             addPlayerHome(player, homename);
                         }
@@ -72,18 +79,26 @@ public class Homes implements CommandExecutor{
                         player.sendMessage("§4§lThis command is currently disabled, if you wish to override this command you are free to do.");
                         return true;
                     }
+                    if (args.length >= 1) {
+                        String homename = args[0];
+                        removePlayerHome(player, homename);
+                    } else {
+                        player.sendMessage("§a§lHere is a list of your homes.");
+                        listhomes(player);
+                    }
                 }
 
             }
         } else {
-            sender.sendMessage("Only players can use this command.");
+            sender.sendMessage("§4§lOnly players can use this command.");
         }
         return true;
     }
 
     private void createHomesFile( ) {
+        File homeConfigFile = new File(this.plugin.getDataFolder(), "homes.yml");
         ifItExists();
-        File homeConfigFile = new File(plugin.getDataFolder(), "homes.yml");
+
         try {
             homeConfig.load(homeConfigFile);
         } catch (IOException | InvalidConfigurationException e) {
@@ -92,16 +107,27 @@ public class Homes implements CommandExecutor{
     }
 
     public void ifItExists( ) {
-        File homeConfigFile = new File(plugin.getDataFolder(), "homes.yml");
+        File homeConfigFile = new File(this.plugin.getDataFolder(), "homes.yml");
         if (! homeConfigFile.exists()) {
-            homeConfigFile.getParentFile().mkdirs();
-            plugin.saveResource("homes.yml", false);
+            plugin.saveResource("homes.yml", true);
         }
+    }
+
+    public void removePlayerHome(Player p, String name) {
+
+        if (! homeConfig.contains("Homes.Players." + p.getUniqueId() + "." + name)) {
+            p.sendMessage("§4§lCould not find your home, " + name + " use /homes to show you a list of your homes!");
+        } else {
+            homeConfig.set(("Homes.Players." + p.getUniqueId() + "." + name), null);
+            p.sendMessage("§4§lYour home, " + name + " has been removed!");
+        }
+
+        load();
     }
 
     public void addPlayerHome(Player p, String name) {
 
-        File homeConfigFile = new File(plugin.getDataFolder(), "homes.yml");
+
         if (homeConfig.contains("Homes.Players." + p.getUniqueId() + "." + name)) {
             p.sendMessage("§4§lYour home, " + name + "already exists! Use /delhome to remove it!");
         } else {
@@ -111,9 +137,9 @@ public class Homes implements CommandExecutor{
             homeConfig.createSection("Homes.Players." + p.getUniqueId() + "." + name + ".Z");
             homeConfig.createSection("Homes.Players." + p.getUniqueId() + "." + name + ".Enabled");
 
-            double x = p.getLocation().getX();
-            double y = p.getLocation().getY();
-            double z = p.getLocation().getZ();
+            int x = (int) p.getLocation().getX();
+            int y = (int) p.getLocation().getY();
+            int z = (int) p.getLocation().getZ();
 
             homeConfig.set("Homes.Players." + p.getUniqueId() + "." + name + ".X", x);
             homeConfig.set("Homes.Players." + p.getUniqueId() + "." + name + ".Y", y);
@@ -128,8 +154,11 @@ public class Homes implements CommandExecutor{
 //    }
 
     public void listhomes(Player p) {
-        Set<String> e = homeConfig.getConfigurationSection("Homes.Players." + p.getUniqueId()).getKeys(false);
-        p.sendMessage(e + "");
+        StringJoiner message = new StringJoiner(", ");
+        for (String key : homeConfig.getConfigurationSection("Homes.Players." + p.getUniqueId()).getKeys(false)) {
+            message.add(key);
+        }
+        p.sendMessage(message + "");
     }
 
     public void checkifplayerhashome(Player p, String name) {
@@ -140,7 +169,8 @@ public class Homes implements CommandExecutor{
     }
 
     public void load( ) {
-        File homeConfigFile = new File(plugin.getDataFolder(), "homes.yml");
+        File homeConfigFile = new File(this.plugin.getDataFolder(), "homes.yml");
+
         try {
             homeConfig.save(homeConfigFile);
         } catch (IOException e) {
@@ -151,18 +181,19 @@ public class Homes implements CommandExecutor{
         } catch (IOException | InvalidConfigurationException e) {
             Bukkit.getConsoleSender().sendMessage("§4§lAn error occurred while reading your home.yml file, if you need help just join our discord with /betterserver help!");
         }
+
+
     }
 
     public void teleportplayertolocation(Player p, String name) {
-        File homeConfigFile = new File(plugin.getDataFolder(), "homes.yml");
-
+        load();
         World world = p.getWorld();
-        Integer x = (int) homeConfig.get("Homes.Players." + p.getUniqueId() + "." + name + ".X");
-        Integer y = (int) homeConfig.get("Homes.Players." + p.getUniqueId() + "." + name + ".Y");
-        Integer z = (int) homeConfig.get("Homes.Players." + p.getUniqueId() + "." + name + ".Z");
+        int x = (int) homeConfig.get("Homes.Players." + p.getUniqueId() + "." + name + ".X");
+        int y = (int) homeConfig.get("Homes.Players." + p.getUniqueId() + "." + name + ".Y");
+        int z = (int) homeConfig.get("Homes.Players." + p.getUniqueId() + "." + name + ".Z");
         p.teleport(new Location(world, x, y, z));
-        p.sendMessage("§e§lSuccesfully teleported you to your home \"" + name.toString() + "\".");
+        p.sendMessage("§e§lSuccesfully teleported you to your home, §a§l" + name + "§e§l.");
 
-   */
+
     }
 }
